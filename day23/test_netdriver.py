0c109a13230d78142052ce1c68dab78df7675d3f
C. E. Sharp <chrisesharp@gmail.com>
2020-02-21 16:58:42 +0000

    refactor tests

--- day23/test_netdriver.py
+++ day23/test_netdriver.py
@@ -4,13 +4,11 @@ from nic import NIC, Network
 
 class NetTest(unittest.TestCase):
     def test_nic(self):
-        nic = NIC(0)
         nw = Network()
+        nic = NIC(0)
         nic.connect(nw)
-        nic.append(1)
-        nic.append(2)
-        nic.append(3)
-        nic.append(4)
+        nic.send(0,[1,2])
+        nic.send(0,[3,4])
         self.assertEqual(0,nic.pop())
         self.assertEqual(1,nic.pop())
         self.assertEqual(2,nic.pop())
@@ -20,30 +18,27 @@ class NetTest(unittest.TestCase):
         self.assertEqual(-1,nic.pop())
     
     def test_nic_2(self):
+        nw = Network()
         nic_zero = NIC(0)
         nic_one = NIC(1)
-        nw = Network()
         nic_zero.connect(nw)
         nic_one.connect(nw)
-        nic_zero.append(1)
-        nic_zero.append(2)
-        nic_one.append(3)
-        nic_one.append(4)
-        self.assertEqual(0,nic_zero.pop())
+        nic_zero.send(1,[2,3])
+        nic_one.send(0,[4,5])
         self.assertEqual(1,nic_one.pop())
-        self.assertEqual(2,nic_zero.pop())
-        self.assertEqual(3,nic.pop())
-        self.assertEqual(4,nic.pop())
-        self.assertEqual(-1,nic.pop())
-        self.assertEqual(-1,nic.pop())
+        self.assertEqual(2,nic_one.pop())
+        self.assertEqual(3,nic_one.pop())
+        self.assertEqual(0,nic_zero.pop())
+        self.assertEqual(4,nic_zero.pop())
+        self.assertEqual(5,nic_zero.pop())
+        self.assertEqual(-1,nic_one.pop())
+        self.assertEqual(-1,nic_zero.pop())
 
-    # def test_net_program(self):
-    #     net = NetDriver("input.txt")
-    #     network = Network()
-    #     net.connect(1, network)
-    #     output = net.read_msg()
-    #     expected = ()
-    #     self.assertEqual(output, expected)
+    def test_net_program(self):
+        network = Network()
+        nd = NetDriver("input.txt").connect(10, network)
+        self.assertEqual(10, nd.nic.pop())
+        self.assertEqual([], network.queues[10])
 
 if __name__ == '__main__':
     unittest.main()
