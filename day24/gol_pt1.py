fb061fc1ab30ab3d56159c8fad023d88ca75f2f5
C. E. Sharp <chrisesharp@gmail.com>
2020-02-25 15:22:42 +0000

    refactor part 1

--- day24/gol_pt1.py
+++ day24/gol_pt1.py
@@ -1,8 +1,11 @@
 import time
 
+icons = { True:"#", False:"." }
+
 class GameOfLife(object):
     def __init__(self, input):
         (self.max_x, self.max_y, alive_cells) = parse(input)
+        self.all_cells = [(x,y) for y in range(self.max_y) for x in range(self.max_x)]
         self.cells = self.serialize(alive_cells)
         self.patterns = set([int(self.cells[-1::-1],2)])
     
@@ -20,11 +23,11 @@ class GameOfLife(object):
         return self.survivors().union(self.births())
     
     def serialize(self, cells):
-        bin_value = ""
-        for y in range(self.max_y):
-            for x in range(self.max_x):
-                bin_value += str(int((x,y) in cells))
-        return bin_value
+        return "".join([str(int(cell in cells)) for cell in self.all_cells])
+
+    def deserialize(self, cells):
+        tmp = list(cells)
+        return set([cell for cell in self.all_cells if int(tmp.pop(0))])
         
     def survivors(self):
         cells = self.deserialize(self.cells)
@@ -32,12 +35,7 @@ class GameOfLife(object):
     
     def births(self):
         cells = self.deserialize(self.cells)
-        births = set()
-        for cell in cells:
-            for deadNeighbour in self.deadNeighbours(cell):
-                if len(self.liveNeighbours(deadNeighbour)) in [1, 2]:
-                    births.add(deadNeighbour)
-        return births
+        return set([deadNeighbour for cell in cells for deadNeighbour in self.deadNeighbours(cell) if len(self.liveNeighbours(deadNeighbour)) in [1, 2]])
     
     def neighbourhood(self, cell):
         deltas = [ (-1, 0), (0,-1), (1, 0), (0, 1) ]
@@ -53,32 +51,13 @@ class GameOfLife(object):
     
     def deadNeighbours(self, cell):
         return self.neighbours(cell, False)
-    
-    def deserialize(self, cells):
-        tmp = cells[:]
-        live_cells = set()
-        x = y = 0
-        while tmp:
-            cell = tmp[0]
-            tmp = tmp[1:]
-            if cell == "1":
-                live_cells.add((x,y))
-            x += 1
-            if x == self.max_x:
-                x = 0
-                y += 1
-        return live_cells
 
     def print(self, level):
         alive_cells = self.deserialize(self.cells)
         output = ""
-        for y in range(self.max_y):
-            for x in range(self.max_x):
-                if (x,y) in alive_cells:
-                    output += "#"
-                else:
-                    output += "."
-            output+="\n"
+        for (x,y) in self.all_cells:
+            output += icons[(x,y) in alive_cells]
+            if x == self.max_x-1: output += "\n"
         return output
 
 def parse(input):
